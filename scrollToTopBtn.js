bf58cd32d8e41e3ec5389a9e74a0b3b1bae624d1
Theodoros Andronikou <95139398+TheodoreAndronikou@users.noreply.github.com>
2024-01-22 20:24:08 +0200

    Delete scrollToTopBtn.js

--- scrollToTopBtn.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function() {
-    document.getElementById("scrollBtn").style.display = window.scrollY > 500 ? "block" : "none";
-};
-
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior:"smooth" });
-}
